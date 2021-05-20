a7d732efea7ef54f03949f09f08579da05b6dc75
Ubuntu <ubuntu@ip-172-31-49-223.ec2.internal>
2021-05-20 00:12:49 +0000

    added rgb_values.py

--- rgb_values.py
+++ rgb_values.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-# Created by: Mr. Coxall
-# Created on: Oct 2019
+# Created by: Jenoe Balote
+# Created on: May 2021
 # This program shows all RGB values
 
 
@@ -12,9 +12,9 @@ def main():
     loop3 = 0
 
     # process & output
-    for loop_counter1 in range(255):
-        for loop_counter2 in range(255):
-            for loop_counter3 in range(255):
+    for loop1 in range(255):
+        for loop2 in range(255):
+            for loop3 in range(255):
                 print("RGB ({0},{1},{2})".format(loop1, loop2, loop3))
 
 
